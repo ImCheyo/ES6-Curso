4109a067e8e0b4dcb8ce50b51a31505b78f21c41
Beneharo <beneharodm@gmail.com>
2023-08-28 11:15:27 +0100

    Modulos

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import { mensajeAlerta, saludo } from "./modulos/mensajeAlerta";
+import Persona from "./modulos/clasePersona";
+
+const beneharo = new Persona('Beneharo Bentor', 21);
+
+beneharo.mostrarInfo();
---
+++ src/modulos/mensajeAlerta.js
@@ -0,0 +1,9 @@
+const mensajeAlerta = (mensaje) => {
+    alert(mensaje);
+}
+
+const saludo = () => {
+    console.log('Saludo!');
+}
+
+export {mensajeAlerta, saludo};
